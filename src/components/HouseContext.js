f90ee9ce54b98c983effe7dc8386b98a7f8eaa00
M Nurhayadi Hasanudin <mnurhayadi1002@gmail.com>
2023-07-21 15:56:16 +0700

    first commit

---
+++ src/components/HouseContext.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, createContext } from "react";
+import { housesData } from "../data";
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (any)");
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState("Property type (any)");
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState("Price range (any)");
+  const [loading, setLoading] = useState(false);
+
+  //Country
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+    const uniqueCountries = ["Location (any)", ...new Set(allCountries)];
+    setCountries(uniqueCountries);
+  }, []);
+
+  //House
+  useEffect(() => {
+    const allProperties = houses.map((house) => {
+      return house.type;
+    });
+    const uniqueProperties = ["Location (any)", ...new Set(allProperties)];
+    setProperties(uniqueProperties);
+  }, []);
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountry,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+        handleClick,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
+};
+
+export default HouseContextProvider;
